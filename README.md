4b7e64f89e7e5421edd5e2409314faa31ef01b08
Pankaj Rane <pankaj.rane@whitehatjr.com>
2021-07-23 18:03:30 +0530

    Deleting the files to upload new build

--- README.md
+++
@@ -1,2 +0,0 @@
-# PitchDetective_Trial
-Trial Music Game
--- src/settings.0da36.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/PD_Level5.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"69b9a",main:"2f28e"}};
